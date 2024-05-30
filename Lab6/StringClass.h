21ec4065c9ead4d4818815a1c2ea74fa8224fb6f
Featattach <featattach@gmail.com>
2024-05-30 14:00:32 +0500

    Добавьте файлы проекта.

---
+++ Lab6/StringClass.h
@@ -0,0 +1,29 @@
+#ifndef STRINGCLASS_H
+#define STRINGCLASS_H
+
+#include <iostream>
+
+class StringClass {
+private:
+    char* str;
+    size_t length;
+
+public:
+    // Конструкторы
+    StringClass(); // Конструктор по умолчанию
+    StringClass(const char* s); // Конструктор с параметрами
+    StringClass(const StringClass& other); // Конструктор копирования
+    ~StringClass(); // Деструктор
+
+    // Методы
+    void print() const;
+    size_t getLength() const;
+
+    // Операторы
+    StringClass& operator=(const StringClass& other);
+    StringClass operator+(const StringClass& other) const;
+    bool operator==(const StringClass& other) const;
+    bool operator!=(const StringClass& other) const;
+};
+
+#endif
